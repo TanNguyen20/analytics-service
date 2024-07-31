401e7babfdcbca1e201966cd7fbba292ee83cba1
TanNguyen20 <nguyennhattan1562000@gmail.com>
2024-07-31 22:25:57 +0700

    add openapi doc

--- src/main/java/vn/tannguyen/analytics_service/AnalyticsServiceApplication.java
+++ src/main/java/vn/tannguyen/analytics_service/AnalyticsServiceApplication.java
@@ -1,11 +1,16 @@
 package vn.tannguyen.analytics_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Analytics API", version = "1.0", description = "Documentation Analytics API v1.0")
+)
 public class AnalyticsServiceApplication {
 
 	public static void main(String[] args) {
